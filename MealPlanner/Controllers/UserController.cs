340b019814238d9f9b5854d9c32b2fe5ca5ff0ba
Unknown <vandana9861@gmail.com>
2019-04-12 19:30:31 -0400

    Initial commit for ASP.NET MVC Meal Planner

---
+++ MealPlanner/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MealPlanner.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void Register()
+        {
+
+        }
+
+        [HttpPost]
+        public void SignIn()
+        {
+
+        }
+
+        public void SignOut()
+        {
+
+        }
+    }
+}
---
+++ MealPlanner/Scripts/meal-planner.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    $('input:radio[name=weight]').change(function () {
+        console.log("weight input element changed.");
+    });
+
+    $('input:radio[name=gender]').change(function () {
+        console.log("gender input element changed.");
+    });
+
+});
