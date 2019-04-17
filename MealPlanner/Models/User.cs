8f3992ac857be3af83d2830b4c82c65ed801755c
Unknown <vandana9861@gmail.com>
2019-04-16 23:15:48 -0400

    Backend logic to register

---
+++ MealPlanner/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MealPlanner.Models
+{
+    [Serializable]
+    public class User
+    {
+        [JsonProperty("fname")]
+        public string FirstName { get; set; }
+        [JsonProperty("lname")]
+        public string LastName { get; set; }
+        [JsonProperty("age")]
+        public int Age { get; set; }
+        [JsonProperty("userId")]
+        public int UserId { get; set; }
+        [JsonProperty("password")]
+        public string Password { get; set; }
+        [JsonProperty("email")]
+        public string Email { get; set; }
+        [JsonProperty("confirmPassword")]
+        public string ConfirmPassword { get; set; }
+        [JsonProperty("gender")]
+        public bool Gender { get; set; }
+        [JsonProperty("bodyStats")]
+        public BodyStats BodyStats { get; set; }
+    }
+}
